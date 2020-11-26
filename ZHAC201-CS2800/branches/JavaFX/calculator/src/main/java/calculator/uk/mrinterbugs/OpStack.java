05174b63b2d11bf8c8b9aab5cd7e5662d86794f4
zhac201 <zhac201@ef38a959-bee9-4a91-a947-c31887e8163c>
2020-11-26 11:26:41 +0000

    Created and tested the number, string and symbol stack classes.

---
+++ ZHAC201-CS2800/branches/JavaFX/calculator/src/main/java/calculator/uk/mrinterbugs/OpStack.java
@@ -0,0 +1,57 @@
+package calculator.uk.mrinterbugs;
+
+/**
+ * An extension of the stack class for storing integers.
+ * 
+ * @author Student
+ *
+ */
+public class OpStack {
+
+
+  private Stack stack;
+  /**
+   * Creates a new stack which can be used.
+   */
+  public OpStack() {
+    this.stack = new Stack();
+  }
+
+  /**
+   * Checks to see if the stack is empty.
+   * 
+   * @return true if empty or false otherwise.
+   */
+  public boolean isEmpty() {
+    if (this.stack.getSize() == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * This method can be called to get the value at the top of the stack.
+   * 
+   * @return The Entry at the top of the stack.
+   * @throws EmptyStack The empty stack exception is thrown if no item in the stack is found.
+   */
+  public Symbol pop() throws EmptyStack {
+    if (this.stack.getSize() == 0) {
+      throw new EmptyStack("The stack is empty");
+    } else {
+      return stack.pop().getOther();
+    }
+  }
+
+  /**
+   * This method is used to store a new value at the top of the stack.
+   * 
+   * @param i The Entry that needs to be stored in the stack.
+   */
+  public void push(Symbol i) {
+    Entry entry = new Entry(i);
+    this.stack.push(entry);;
+  }
+
+}
