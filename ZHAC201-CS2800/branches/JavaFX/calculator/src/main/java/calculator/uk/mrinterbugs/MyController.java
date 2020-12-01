bc1dc0adb8a2b438700209f73aa77fda868e08c2
zhac201 <zhac201@ef38a959-bee9-4a91-a947-c31887e8163c>
2020-12-01 20:14:49 +0000

    Gets the user input from the javafx gui.

--- ZHAC201-CS2800/branches/JavaFX/calculator/src/main/java/calculator/uk/mrinterbugs/MyController.java
+++ ZHAC201-CS2800/branches/JavaFX/calculator/src/main/java/calculator/uk/mrinterbugs/MyController.java
@@ -2,11 +2,12 @@ package calculator.uk.mrinterbugs;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.Event;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 /**
- * belh.
+ * Controller class for my javaFX code.
  * 
  * @author Student
  *
@@ -19,20 +20,24 @@ public class MyController {
   @FXML // URL location of the FXML file that was given to the FXMLLoader
   private URL location;
 
-  @FXML
-  void aboutProgram(Event event) {
+  @FXML //The textfield used to get an input
+  private TextField textfield;
+
+  @FXML //Tells the user which version of the program is being used.
+  void aboutProgram(ActionEvent event) {
     System.out.println("Button clicked");
   }
 
-  @FXML
-  void getInput(Event event) {
-    System.out.println("Button clicked 2");
+  @FXML //Gets and returns the answer to the user.
+  void getInput(ActionEvent event) {
+    Integer value1 = Integer.valueOf(textfield.getText());
+    Integer r = value1 + 5;
+    textfield.setText(r.toString());
   }
 
   @FXML // This method is called by the FXMLLoader when initialization is complete
   void initialize() {
     System.out.println("Initialized JavafxController");
   }
-
 }
 
