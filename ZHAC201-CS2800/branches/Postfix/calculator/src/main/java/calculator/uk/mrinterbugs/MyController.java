1837539b340635811d4365ad3ee5ecd8cd7382b6
zhac201 <zhac201@ef38a959-bee9-4a91-a947-c31887e8163c>
2020-12-03 14:08:14 +0000

    updated controller to use postfix method.

--- ZHAC201-CS2800/branches/Postfix/calculator/src/main/java/calculator/uk/mrinterbugs/MyController.java
+++ ZHAC201-CS2800/branches/Postfix/calculator/src/main/java/calculator/uk/mrinterbugs/MyController.java
@@ -20,19 +20,25 @@ public class MyController {
   @FXML // URL location of the FXML file that was given to the FXMLLoader
   private URL location;
 
-  @FXML //The textfield used to get an input
+  @FXML // The textfield used to get an input
   private TextField textfield;
 
-  @FXML //Tells the user which version of the program is being used.
+  @FXML // Tells the user which version of the program is being used.
   void aboutProgram(ActionEvent event) {
     System.out.println("Button clicked");
   }
 
-  @FXML //Gets and returns the answer to the user.
-  void getInput(ActionEvent event) {
-    Integer value1 = Integer.valueOf(textfield.getText());
-    Integer r = value1 + 5;
-    textfield.setText(r.toString());
+  @FXML // Gets and returns the answer to the user.
+  private void getInput(ActionEvent event) throws UserInput {
+    String test = textfield.getText();
+    Float result = null;
+    try {
+      result = PostfixCalculator.evaluate(test);
+    } catch (UserInput | EmptyStack e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    textfield.setText(result.toString());
   }
 
   @FXML // This method is called by the FXMLLoader when initialization is complete
